ed305ebc75bd18420b93a9626da04b4fdf5354f6
João Duarte <jaduarte1998@gmail.com>
2022-07-23 00:10:46 +0100

    Fix border order and start distributing stones

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,47 +1,63 @@
 var express = require('express');
 var router = express.Router();
 
+// 1 = p1, 0 = p2, made to simplify changing players
 let player1 = true;
-let startingStones, pits, board;
+let startingStones, pits, board, bigPit1, bigPit2;
 
-router.get('/', function(req, res) {
-    // Disable cache to test
-    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-    res.header("Pragma", "no-cache");
-    res.header("Expires", 0);
+router.get('/', function (req, res) {
+  // Disable cache to test
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.header('Pragma', 'no-cache');
+  res.header('Expires', 0);
 
-    pits = req.query.pits;
-    stones = req.query.stones;
-    PrepareBoard();
-    
-    res.json({board, player1});
+  pits = req.query.pits;
+  startingStones = req.query.stones;
+  bigPit1 = pits;
+  bigPit2 = pits * 2 + 1;
+  PrepareBoard();
+
+  res.json({ board, player1 });
 });
 
-router.get('/play', function(req, res) {
-    // Disable cache to test
-    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-    res.header("Pragma", "no-cache");
-    res.header("Expires", 0);
+router.get('/play', function (req, res) {
+  // Disable cache to test
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.header('Pragma', 'no-cache');
+  res.header('Expires', 0);
 
-    PlayTurn(req.query.pit);
-    
-    res.json({board, player1});
+  PlayTurn(req.query.pit);
+
+  res.json({ board, player1 });
 });
 
-// Creates an array with x pits for each player plus 2 big pits, and initializes respective values 
+// Creates an array with x pits for each player plus 2 big pits, and initializes respective values
 function PrepareBoard() {
-    board = Array((pits * 2) + 2).fill(startingStones);
-    board[pits] = 0;
-    board[(pits * 2) + 1] = 0;
+  board = Array(pits * 2 + 2).fill(startingStones);
+  board[bigPit1] = 0;
+  board[bigPit2] = 0;
 }
 
 function PlayTurn(pit) {
-    const stonesToMove = board[pit];
-    let currentPit = pit;
+  const stonesToMove = board[pit];
+  let currentPit = pit;
+  board[currentPit] = 0;
 
-    for (let stonesMoved = 0; stonesMoved < stonesToMove; stonesMoved++) {
+  for (let stonesMoved = 0; stonesMoved < stonesToMove; stonesMoved++) {
+    currentPit++;
 
-    }
+    // Skips over the big pits of the other player
+    if (
+      (board[currentPit] === bigPit1 && !player1) ||
+      (board[currentPit] === bigPit2 && player1)
+    )
+      continue;
+
+    board[currentPit]++;
+
+    // If it reaches the end of the board goes back to the beggining
+    if (currentPit === board.length - 1) currentPit = 0;
+  }
 }
 
 module.exports = router;
